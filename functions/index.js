5f7d3fcd9e3b5335d8bfef045b15030974756e01
zohaib younis <zabi.jarral08@gmail.com>
2019-10-21 01:33:26 +0200

    Login and sigu up endpoint have been added

--- functions/index.js
+++ functions/index.js
@@ -63,6 +63,16 @@ app.post('/scream', (req, res) => {
         })
 })
 
+const isEmail = email => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(regex)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if (string.trim() === '') return true;
+    else return false
+}
 
 // Signup Route
 app.post('/signup', (req, res) => {
@@ -72,6 +82,21 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle,
     };
+
+    let errors = {};
+
+
+    if (isEmpty(newUser.email)) {
+        errors.email = "Email must not be empty"
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Password must match"
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+    if (Object.keys.length > 0) return res.status(400).json(errors)
     //should not have duplicate user handle
     let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
@@ -112,5 +137,30 @@ app.post('/signup', (req, res) => {
 
 })
 
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = "Must not be empty";
+    if (isEmpty(user.password)) errors.password = "Must not be empty";
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        }).then(token => {
+            return res.json(token)
+        }).catch(err => {
+            console.log(err)
+            return res.status(500).json({
+                error: err.code
+            })
+        })
+})
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
